0841b823681f7c898c526a777ded3972f6087668
Prent Rodgers <prent@ripnread.com>
2023-01-03 19:09:39 -0500

    Delete convert-eps-to-pdf.sh

--- convert-eps-to-pdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# csound creates a text output with graphical waveforms as an .EPS file, which is not searchable like a .PDF file. This does a batch conversion.
-#Batch convert .EPS and .AI files into .PDF format
-
-#EPS to PDF batch convert
-#make sure you downloaded eps-to-pdf
-#To install type 'sudo apt-get install texlive-font-utils' in terminal
-for file in *.eps; do epstopdf --hires "$file"; done
